a52f954cfa624eb43d2c814c03a6832ca29b5214
ManuelRios94 <manuel.ir2302@gmail.com>
2019-10-20 18:35:11 -0300

    Client Controller

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,27 @@
-export const URL_CLIENTS = 'http://www.mocky.io/v2/5808862710000087232b75ac';
+const URL_CLIENTS = 'http://www.mocky.io/v2/5808862710000087232b75ac';
 
-export const URL_POLICIES = 'http://www.mocky.io/v2/580891a4100000e8242b75c5';
+const URL_POLICIES = 'http://www.mocky.io/v2/580891a4100000e8242b75c5';
 
-export const OK = 200;
+const OK = 200;
 
-export const BAD_REQUEST = 400;
+const BAD_REQUEST = 400;
 
-export const UNAUTHORIZED = 401;
+const UNAUTHORIZED = 401;
 
-export const NOT_FOUND = 404;
+const NOT_FOUND = 404;
 
-export const PRECONDITION_FAILED = 404;
+const PRECONDITION_FAILED = 404;
+
+const ROLE_USER = 'user';
+
+const ROLE_ADMIN = 'admin'
+
+module.exports = {
+    URL_CLIENTS,
+    URL_POLICIES,
+    OK,
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    NOT_FOUND,
+    PRECONDITION_FAILED
+}
