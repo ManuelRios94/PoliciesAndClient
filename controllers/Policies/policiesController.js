994dede186a4dde6d9ca12172975ea2a2c32ad0f
ManuelRios94 <manuel.ir2302@gmail.com>
2019-10-20 18:54:57 -0300

    Policies Controller

--- controllers/Policies/policiesController.js
+++ controllers/Policies/policiesController.js
@@ -1,39 +1,53 @@
 const { getClients, getPolicies } = require('../../utils/database');
 const {OK, NOT_FOUND, BAD_REQUEST} = require('../../utils/constants');
 
-const GetPoliciesListByClientName =  (req, res) => {
-    const name = req.params.name;
-    // await getClients()
-    // .then((clients) => {
-    //     const clientsToSearch = clients.filter(clients => clients.name === name);
-    //     if(result !== []){
-    //         res.status(OK).json(result); 
-
-
-
-    //     } else {
-    //         res.status(NOT_FOUND).json(result); 
-    //     }
-    //     // For each clientsToSearch assosiate
-    //     await getPolicies()
-    //     .then((policies) => {
-    //         const result = policies.filter(policies => policies.client_id === client_id);
-    //         res.status(OK).json(result); 
-    //     })
-    //     .catch(() => {
-    //         res.status(BAD_REQUEST);
-    //     }); 
-    // })
-    // .catch(() => {
-    //     res.status(BAD_REQUEST);
-    // });
+const GetPoliciesListByClientName =  async (req, res) => {
+    const clientName = req.params.name;
+    await getClients()
+    .then(async (clients) => {
+        const client = clients.filter(clients => clients.clientName === clientName);
+        if(client !== []){
+            await getPolicies()
+            .then((policies) => {
+                const results = policies.filter(policies => policies.clientId === client[0].id);
+                res.status(OK).json(results); 
+            })
+            .catch(() => {
+                res.status(BAD_REQUEST);
+            }); 
+        } else {
+            res.status(NOT_FOUND); 
+        }
+       
+    })
+    .catch(() => {
+        res.status(BAD_REQUEST);
+    });
 
     
 }
 
-const GetClientByPolicieNumber = (req, res) => {
-    const id = req.params.id;
-    res.status(200).json(id);
+const GetClientByPolicieNumber = async (req, res) => {
+    const policieId = req.params.id;
+    await getPolicies()
+    .then(async (policies) => {
+        const result = policies.filter(policies => policies.id === policieId);
+        if( result !== []){
+            await getClients()
+            .then(async (clients) => {
+                const client = clients.filter(clients => clients.id === result[0].clientId);
+                res.status(OK).json(client); 
+            })
+            .catch(() => {
+                res.status(BAD_REQUEST);
+            });
+        } else {
+            res.status(NOT_FOUND);
+        }
+    })
+    .catch(() => {
+        res.status(BAD_REQUEST);
+    }); 
 }
 
 module.exports = {
--- routes/Policies/policiesRoute.js
+++ routes/Policies/policiesRoute.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const policiesController = require('../../controllers/Policies/policiesController');
 
 /* GET The Policies Linked by Client Name . */
-router.get('/name/:name', policiesController.GetPoliciesListByClientName);
+router.get('/client-name/:name', policiesController.GetPoliciesListByClientName);
 
 /* GET Client Linked by Policie Number . */
 router.get('/id/:id', policiesController.GetClientByPolicieNumber);
